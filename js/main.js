35dfb0c975ba5573b36e3445a804232e00491ea2
PeterDamrosch <peter.damrosch@gmail.com>
2017-05-24 21:37:59 -0400

    cutoff added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ var path = d3.geoPath();
 
 var dataExternal = null;
 
+var topoIDList = []
+
+// Append Div for tooltip to SVG
+var div = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
 d3.csv("data/FHWA State Gas Tax Rates Topo 5-23-17.csv", function(error, data) {
     if (error) throw error;
 
@@ -31,6 +39,7 @@ d3.csv("data/FHWA State Gas Tax Rates Topo 5-23-17.csv", function(error, data) {
         d["2015"] = +d["2015"];
         d["2016"] = +d["2016"];
 
+        topoIDList.push(d["TOPO_ID"]);
         dataDict[d["TOPO_ID"]] = d
     });
 
@@ -51,7 +60,25 @@ d3.csv("data/FHWA State Gas Tax Rates Topo 5-23-17.csv", function(error, data) {
             .attr("d", path)
             .attr("fill", function(d) {
                 return fillState(data, d.id, 2000)
-            });
+            })
+            .on("mouseover", function(d) {
+                // Tooltip
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.text(function(e) {
+                    return updateToolTip(d.id)
+                })
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+
+            // fade out tooltip on mouse out
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });;
 
         svg.append("path")
             .attr("class", "state-borders")
@@ -107,6 +134,7 @@ legend.append("text")
 
 
 
+//////////// SLIDER ////////////
 
 $( function() {
     $( "#slider" ).slider({
@@ -115,23 +143,100 @@ $( function() {
         max: 2016,
         step: 1,
         slide: function( event, ui ) {
-            updateStateColors(ui.value);
-            updateYear(ui.value);
+            globalYear = ui.value;
+            updateStateColors();
+            updateYear();
         },
     });
 });
 
-var updateStateColors = function(baseYear) {
+
+// Update colors
+var updateStateColors = function() {
 
     svg.selectAll("g path")
         .attr("fill", function(d) {
-            return fillState(dataExternal, d.id, baseYear)
+            return fillState(dataExternal, d.id, globalYear)
+        })
+        .attr("class", function(d) {
+            if (globalYear == 2000) {
+                return "state-no-highlight"
+            } else if (!significantDifferential) {
+                "state-no-highlight"
+            } else {
+                currentYearRate = dataExternal[d.id][globalYear];
+                lastYearRate = dataExternal[d.id][globalYear - 1];
+                if (currentYearRate >= lastYearRate + significantDifferential) {
+                    return "state-highlight"
+                } else {
+                    return "state-no-highlight"
+                }
+            }
         })
 };
 
-var updateYear = function(baseYear) {
-    titleText = "State Gas Taxes | " + baseYear.toString();
-    console.log(titleText);
-    d3.select("#title-name").html(titleText)
+// Global variable for year + changes
+var globalYear = 2000;
+var significantDifferential = null;
+var totalChanges = 50;
+
+var yearList = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"]
+
+// Update year
+var updateYear = function() {
+    titleText = globalYear.toString();
+    d3.select("#sidebar-title").html(titleText)
 }
 
+
+//////////// TOOLTIP ////////////
+
+var updateToolTip = function(stateID) {
+
+    stateName = dataExternal[stateID]["State"];
+    taxAmount = dataExternal[stateID][globalYear];
+    toolTipText = stateName + ": " + taxAmount;
+    return toolTipText
+}
+
+
+
+////////// CUTOFF Level /////////
+$( function() {
+    $( 'select[name=cutoff]').change(function() {
+        chosenLevel = $(this).val();
+        significantDifferential = parseFloat(chosenLevel);
+        updateTotalChanges();
+        //significantDifferential = $(this).val();
+    });
+} );
+
+var updateTotalChanges = function() {
+    var changes = 0;
+
+    for (i = 0; i < (yearList.length - 1); i++) {
+
+        currentYear = yearList[i];
+        nextYear = yearList[i+1];
+
+        for (k in topoIDList) {
+            topoID = topoIDList[k]
+
+            currentValue = dataExternal[topoID][currentYear];
+            nextYearValue = dataExternal[topoID][nextYear];
+
+
+            var differenceNumber = null;
+            if (!significantDifferential) {
+                differenceNumber = 0.001
+            } else {
+                differenceNumber = significantDifferential
+            }
+            if (nextYearValue >= currentValue + differenceNumber) {
+                changes = changes + 1
+            }
+        }
+    };
+    countText = "Total increases: " + changes.toString();
+    d3.select("#sidebar-count").html(countText)
+};
